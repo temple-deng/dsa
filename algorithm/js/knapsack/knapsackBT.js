77ca552ac48178c1f1e156abf7331ff04cf4fcd1
dengbo <dengbo@unitedstack.com>
2018-09-21 18:04:25 +0800

    update

--- algorithm/js/knapsack/knapsackBT.js
+++ algorithm/js/knapsack/knapsackBT.js
@@ -34,7 +34,44 @@ function knapsack(w, v, W) {
   return arr[n][W];
 }
 
+
+/**
+ * 背包问题的一种空间优化方案
+ * 考虑背包问题的递归方程
+ * F(i, c) = max(F(i-1, c), vi + F(i-1, c-wi))
+ * 第 i 行的元素只依赖于第 i-1 行的元素，理论上，只需要保存两行元素
+ * 空间复杂度 O(2 * C) = O(C)
+ * @param {*} w 
+ * @param {*} v 
+ * @param {*} C 
+ */
+function knapsackOpt(w, v, C) {
+  const n = w.length;
+  var arr = Array(2);
+  arr[0] = Array(C+1);
+  arr[1] = Array(C+1);
+
+  for (let i = 0; i <= C; i++) {
+    arr[0][i] = 0;
+  }
+  arr[1][0] = 0;
+
+  for (let i = 1; i <= n; i++) {
+    let index = i % 2;
+    for (let j = 1; j <= C; j++) {
+      if ( j - w[i-1] < 0) {
+        arr[index][j] = arr[(i+1) % 2][j];
+      } else {
+        arr[index][j] = Math.max(arr[(i+1) % 2][j],
+          v[i-1] + arr[(i+1) % 2][j - w[i-1]]); 
+      }
+    }
+  }
+
+  return n % 2 === 1 ? arr[1][C] : arr[0][C];
+}
+
 var w = [2, 1, 3, 2];
 var v = [12, 10, 20, 15];
 
-console.log(knapsack(w, v, 5));
+console.log(knapsackOpt(w, v, 5));
